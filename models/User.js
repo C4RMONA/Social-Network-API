c5483deee20cab5fd37813aba652041dbd38d2e9
chris <christiancarmona3@gmail.com>
2022-08-15 13:02:03 -0500

    created models

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        thought: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+//gets total count of friends
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friends) => total + friends.user.length + 1, 0) 
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
